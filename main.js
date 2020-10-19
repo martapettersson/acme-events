aadd9c8f04bd4d435a21317a4bc39336981e116b
Sebastian Orbom <zebban90a@gmail.com>
2020-10-19 19:16:57 +0200

    changes in divcontent

---
+++ main.js
@@ -0,0 +1,10 @@
+const img1 = document.createElement("img"); 
+img1.src = 'images/kiss.jpg' 
+const text = document.createElement("span")
+text.innerHTML ="tralal lorem looooooooooooooooooorem tetralal lorem looooooooooooooooooorem tetralal lorem looooooooooooooooooorem tetralal lorem looooooooooooooooooorem te"       
+document.getElementById("divContent").appendChild(img1)
+document.getElementById("divContent").appendChild(text);
+
+const img2 = document.createElement("img");
+img2.src = 'images/loreen.jpg'
+document.getElementById("divContent").appendChild(img2);
