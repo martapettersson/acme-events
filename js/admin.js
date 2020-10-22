1c37a97babb08799f2b0bdc97e0a24f6e9f744c9
Axel Thiel <axth2220@gmail.com>
2020-10-22 17:11:42 +0200

    changes

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,15 @@
 document.addEventListener('DOMContentLoaded', e => {
 
+    //TA BORT SEN!
+    document.getElementById('clear').addEventListener('click', e => localStorage.clear())
+
     const category = new Category();
     const arena = new Arena();
 
     document.getElementById('event-submit-btn').addEventListener('click', e => {
 
         AdminLogic.createEvent()
+        AdminUI.clearForm();
         AdminUI.clearTable();
         AdminUI.showEvents();
 
