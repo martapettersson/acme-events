8ce9b720e3aa20642198f27aae2daf1c5ded9935
Axel Thiel <axth2220@gmail.com>
2020-10-26 11:27:46 +0100

    moved the id counter from the eventcontrol class to local storage. bug fixed

--- js/AdminClasses.js
+++ js/AdminClasses.js
@@ -31,19 +31,28 @@ class EventControl {
 	//Klass som skapar nya eventobjekt, gör om dem till JSON-objekt som den lägger till i localstorage
 	//Tar även bort och uppdaterar events som ligger i local storage.
 	constructor() {
-		this.id = 0;
+		if (localStorage.getItem("idCounter") === null) {
+			localStorage.setItem("idCounter", 0);
+		}
 	}
 
 	createEvent() {
+		//skapa ett nytt eventobjekt och lägger in det i localstorage. Uppdaterar id-counter i local storage.
 		let artist = document.getElementById("artist-name").value;
 		let date = document.getElementById("date").value;
 		let category = document.getElementById("category-select").value;
 		let arena = document.getElementById("arena-select").value;
 
 		let event = new Event(artist, date, category, arena);
-		localStorage.setItem(this.id, JSON.stringify(event));
+		localStorage.setItem(
+			localStorage.getItem("idCounter"),
+			JSON.stringify(event)
+		);
 
-		this.id++;
+		localStorage.setItem(
+			"idCounter",
+			parseInt(localStorage.getItem("idCounter")) + 1
+		);
 	}
 
 	removeEventObject(id) {
@@ -53,7 +62,7 @@ class EventControl {
 	editEventObject(id) {
 		//Lägger till informationen från ett eventobjekt i localstorage i form. Sätter sedan en eventlistener på editknappen.
 		//eventlistener-funktionen skapar ett nytt event-objekt och skriver över det gamla objetet i localstorage.
-		//gör sedan spara-knappen osynlig och visar den ursprungliga submit-knappen.
+		//tar sedan bort eventlistenerngör sedan spara-knappen osynlig och visar den ursprungliga submit-knappen.
 		let eventObj = JSON.parse(localStorage.getItem(id));
 		document.getElementById("artist-name").value = eventObj.artistName;
 		document.getElementById("date").value = eventObj.date;
@@ -86,22 +95,24 @@ class AdminUI {
 	//Kontrollerar vad som visar på skärmen i admingränssnittet
 
 	static showEvents() {
-		//Går igenom alla events i localstorage och skriver ut dem i en tabell.
+		//Går igenom alla events i localstorage och skriver ut dem i en tabell. Om key är idCounter hoppar den över
 		let keys = Object.keys(localStorage).sort();
 		for (let key of keys) {
-			let eventObject = JSON.parse(localStorage.getItem(key));
-			let table = document.getElementById("event-table");
-			let tr = document.createElement("tr");
-			tr.id = key;
-			tr.innerHTML += `
+			if (key !== "idCounter") {
+				let eventObject = JSON.parse(localStorage.getItem(key));
+				let table = document.getElementById("event-table");
+				let tr = document.createElement("tr");
+				tr.id = key;
+				tr.innerHTML += `
                 <td>${eventObject.artistName}</td>
                 <td>${eventObject.date}</td>
                 <td>${eventObject.category}</td>
                 <td>${eventObject.arena}</td>
                 <td><span class='delete'>DELETE</span></td> 
                 <td><span class='edit'>EDIT</span></td>
-            `;
-			table.append(tr);
+           		 `;
+				table.append(tr);
+			}
 		}
 	}
 
